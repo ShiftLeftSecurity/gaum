6ea384872db12d416222b58d8ea165e9ed3d6128
Olof-Joachim Frahm <olof@shiftleft.io>
2019-02-27 12:30:30 +0100

    One more chain test.

--- db/chain/chain_test.go
+++ db/chain/chain_test.go
@@ -98,6 +98,15 @@ func TestExpresionChain_Render(t *testing.T) {
 		},
 		{
 			name: "basic selection with distinct",
+			chain: (&ExpresionChain{}).Select(Distinct("field1")).
+				Table("convenient_table").
+				AndWhere("field1 > ?", 1),
+			want:     "SELECT DISTINCT field1 FROM convenient_table WHERE field1 > $1",
+			wantArgs: []interface{}{1},
+			wantErr:  false,
+		},
+		{
+			name: "basic selection with distinct as",
 			chain: (&ExpresionChain{}).Select(As(Distinct("field1"), "renamed")).
 				Table("convenient_table").
 				AndWhere("field1 > ?", 1),
