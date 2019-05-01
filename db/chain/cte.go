e57152b07a6b8eee033efb601b53de367ed9cb41
Horacio Duran <horacio.duran@gmail.com>
2019-05-01 11:38:08 -0300

    Added CTE support to chain (#19)
    I added a very basic support to add [CTEs](https://www.postgresql.org/docs/11/queries-with.html)
    to `SELECT` statements, this covers the mot basic use case.

---
+++ db/chain/cte.go
@@ -0,0 +1,49 @@
+package chain
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+//    Copyright 2019 Horacio Duran <horacio@shiftleft.io>, ShiftLeft Inc.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+// With adds a CTE to your query (https://www.postgresql.org/docs/11/queries-with.html)
+func (ec *ExpresionChain) With(name string, cte *ExpresionChain) *ExpresionChain {
+	if len(ec.ctes) == 0 {
+		ec.ctes = map[string]*ExpresionChain{}
+	}
+	ec.ctes[name] = cte
+	return ec
+}
+
+func (ec *ExpresionChain) renderctes() (string, []interface{}, error) {
+	if len(ec.ctes) == 0 {
+		return "", []interface{}{}, nil
+	}
+	args := []interface{}{}
+	queries := []string{}
+	for name, expr := range ec.ctes {
+		cteQ, cteArgs, err := expr.render(true)
+		if err != nil {
+			return "", nil, errors.Wrapf(err, "rendering cte %s", name)
+		}
+		queries = append(queries, fmt.Sprintf("%s AS (%s)", name, cteQ))
+		args = append(args, cteArgs...)
+	}
+	query := fmt.Sprintf("WITH %s", strings.Join(queries, ", "))
+	return query, args, nil
+}
