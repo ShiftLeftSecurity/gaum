aad75cc1ff434d9f19a00e728eb47925c70a44ee
Horacio Duran <horacio.duran@gmail.com>
2019-07-04 20:16:19 -0300

    Add a way to re-set group by

--- db/chain/chain.go
+++ db/chain/chain.go
@@ -134,6 +134,19 @@ func (ec *ExpresionChain) append(atom querySegmentAtom) {
 	ec.segments = append(ec.segments, atom)
 }
 
+func (ec *ExpresionChain) removeOfType(atomType sqlSegment) {
+	ec.lock.Lock()
+	defer ec.lock.Unlock()
+	newSegments := []querySegment{}
+	for i, s := range ec.segments {
+		if s.segment == atomType {
+			continue
+		}
+		newSegments = append(newSegments, ec.segments[i])
+	}
+	ec.segments = newSegments
+}
+
 func (ec *ExpresionChain) mutateLastBool(operation sqlBool) {
 	ec.lock.Lock()
 	defer ec.lock.Unlock()
@@ -565,6 +578,21 @@ func (ec *ExpresionChain) OrderBy(order *OrderByOperator) *ExpresionChain {
 // further chaining.
 // THIS DOES NOT CREATE A COPY OF THE CHAIN, IT MUTATES IN PLACE.
 func (ec *ExpresionChain) GroupBy(expr string, args ...interface{}) *ExpresionChain {
+	ec.append(
+		querySegmentAtom{
+			segment:   sqlGroup,
+			expresion: expr,
+			arguments: args,
+			sqlBool:   SQLNothing,
+		})
+	return ec
+}
+
+// GreedyGroupBy adds a 'GROUP BY' to the 'ExpresionChain' and returns the same chan to facilitate
+// further chaining, this version of group by removes all other group by entries.
+// THIS DOES NOT CREATE A COPY OF THE CHAIN, IT MUTATES IN PLACE.
+func (ec *ExpresionChain) GreedyGroupBy(expr string, args ...interface{}) *ExpresionChain {
+	ec.removeOfType(sqlGroup)
 	ec.append(
 		querySegmentAtom{
 			segment:   sqlGroup,
